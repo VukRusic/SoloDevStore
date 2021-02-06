6e0338224812a4ab5e9f06062922cde237d38228
luka2118@its.edu.rs <luka2118@its.edu.rs>
2021-02-06 19:51:44 +0100

    Izmenjen view registracija i metoda adduser da bi radila sa novom bazom

--- Solo.EntityFramework/SoloEntities.Designer.cs
+++ Solo.EntityFramework/SoloEntities.Designer.cs
@@ -1,4 +1,4 @@
-﻿// T4 code generation is enabled for model 'C:\Users\Lenovo T470p\Desktop\Solo\MVC\Solo\Solo.EntityFramework\SoloEntities.edmx'. 
+﻿// T4 code generation is enabled for model 'D:\programi c#\SOLO\Solo.EntityFramework\SoloEntities.edmx'. 
 // To enable legacy code generation, change the value of the 'Code Generation Strategy' designer
 // property to 'Legacy ObjectContext'. This property is available in the Properties Window when the model
 // is open in the designer.
